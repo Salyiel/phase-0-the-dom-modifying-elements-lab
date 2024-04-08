dea8f9b4bc9b96640749480f8b02b5d8a5bdece2
Stacy <salyiela@icloud.com>
2024-04-08 14:26:51 +0300

    Complete lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+/* Part 1 */
+document.querySelector('main')
+
+main.remove()
+
+/* Part 2 */
+//const newHeader = document.querySelector('h1#victory');
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory';
+//const id = document.createElementById.append('')
+
+/* Part 3 */
+//newHeader.textContent = "Jacob is the champion";
+
+const victory = document.getElementById("victory");
+newHeader.innerHTML = "Jacob is the champion";
